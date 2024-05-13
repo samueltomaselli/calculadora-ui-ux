c8aa897f6f965eda72f60e34ff88cfac289dd5ff
Samuel Henrique Tomaselli <samueltomaselli2005@gmail.com>
2024-05-12 21:02:04 -0300

    Ãinit

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
-const result = document.querySelector(".result");
-
-function handleError() {
-  const currentResult = result.innerHTML.trim();
-  if (currentResult === "Erro") {
-    result.innerHTML = "";
-  }
-}
-
-function add(valor) {
-  const operators = ["/", "+", "-", "*"];
-  handleError();
-  const currentResult = result.innerHTML.trim();
-
-  if (
-    (operators.includes(valor) && currentResult === "" && valor !== "-") ||
-    (operators.includes(valor) && operators.includes(currentResult.slice(-1)))
-  ) {
+document.getElementById("inputForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const primeiroNumero = parseFloat(document.getElementById("primeiroNumero").value);
+  const segundoNumero = parseFloat(document.getElementById("segundoNumero").value);
+  const operacao = document.getElementById("operacao").value;
+  if (isNaN(primeiroNumero) || isNaN(segundoNumero) || primeiroNumero < 0 || segundoNumero < 0) {
+    alert("Por favor, insira números positivos.");
     return;
   }
+  const resultado = calcular(primeiroNumero, segundoNumero, operacao);
+  mostrarResultado(resultado);
+  fecharModalEntrada();
+});
 
-  result.innerHTML += valor;
-}
-
-function clean() {
-  result.innerHTML = "";
-}
-
-function backspace() {
-  handleError();
-
-  if (result.innerHTML) {
-    result.innerHTML = result.innerHTML.trim().slice(0, -1);
+function calcular(a, b, operacao) {
+  switch (operacao) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return b !== 0 ? a / b : "Não é possível  dividir por zero";
+    default:
+      return "Operação inválida";
   }
 }
 
-function confirm() {
-  try {
-    result.innerHTML = eval(result.innerHTML) || "";
-  } catch (e) {
-    result.innerHTML = "Erro";
-  }
+function abrirModalEntrada() {
+  document.getElementById("entradaModal").style.display = "block";
 }
+
+function fecharModalEntrada() {
+  document.getElementById("entradaModal").style.display = "none";
+}
+
+function mostrarResultado(resultado) {
+  document.getElementById("resultadoTexto").textContent = `Resultado: ${resultado}`;
+  document.getElementById("resultadoModal").style.display = "block";
+}
+
+window.onclick = function (event) {
+  if (event.target.classList.contains("modal")) {
+    event.target.style.display = "none";
+  }
+};
+
+document.querySelectorAll(".close-button").forEach((button) => {
+  button.onclick = function () {
+    button.closest(".modal").style.display = "none";
+  };
+});
